6967d0d13efb52d4e8935b88228975dc6de53d9d
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-10-12 23:13:51 +0900

    스피디 피드백 3: 모바일에서 헤더 이상하게 보이는 문제 수정 	modified:   src/App.tsx 	modified:   src/style.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet, Link, useLocation } from 'react-router-dom';
 import { useTranslation } from "react-i18next";
+import { useState } from 'react';
 import TranslateButton from './language/translate_button.tsx';
 import './style.scss';
 
@@ -7,16 +8,26 @@ function App() {
   const location = useLocation();
   const { t } = useTranslation();
   const path = location.pathname;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div>
       <header>
         <nav className="container">
           <div className="logo">ISLAND</div>
-          <div className="nav-menu">
-            <Link to="/" className={"nav-link "+(path=="/"?"active":"")}>{t("home")}</Link>
-            <Link to="/submission" className={"nav-link "+(path=="/submission"?"active":"")}>{t("submit_id")}</Link>
-            <Link to="/about" className={"nav-link "+(path=="/about"?"active":"")}>{t("about_island")}</Link>
+          <div className={`hamburger${isMenuOpen ? ' active' : ''}`} onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={`nav-menu${isMenuOpen ? ' active' : ''}`}>
+            <Link to="/" className={"nav-link "+(path=="/"?"active":"")} onClick={() => setIsMenuOpen(false)}>{t("home")}</Link>
+            <Link to="/submission" className={"nav-link "+(path=="/submission"?"active":"")} onClick={() => setIsMenuOpen(false)}>{t("submit_id")}</Link>
+            <Link to="/about" className={"nav-link "+(path=="/about"?"active":"")} onClick={() => setIsMenuOpen(false)}>{t("about_island")}</Link>
             <TranslateButton></TranslateButton>
           </div>
         </nav>
@@ -28,12 +39,9 @@ function App() {
       <footer>
         <div className="footer-content">
           <p className="social">
-            <a href="https://soundcloud.com/island-promotion" target="_blank">SoundCloud</a>·
+            <a href="https://soundcloud.com/island-by-rrayy" target="_blank">SoundCloud</a>·
             <a href="https://discord.gg/B55PyFm7nW" target="_blank">Discord</a>·
             <a href="https://instagram.com/island.ids" target="_blank">Instagram</a>
-          </p>
-          <p className="contact">
-            <a href="mailto:contact@islandlabel.com">contact@islandlabel.com</a>
           </p>
           <p className="license">
             © 2025 ISLAND · MIT License
